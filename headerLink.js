7e2ad1f0753711e8e897b77427102fa253ab9d4b
Fateyes <572582091@qq.com>
2017-04-24 20:15:48 +0800

    combine files

---
+++ headerLink.js
@@ -0,0 +1,18 @@
+/**
+ * Created by fx50 on 2017/4/24.
+ */
+angular.module('header',['ngRoute'])
+    .controller('headerCtrl',function () {
+
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/',{templateUrl:'b.html'})
+            .when('/themeMarket',{templateUrl:'themeMarket.html'})
+            .when('/themeClass',{templateUr:'themeClass.html'})
+            .when('/studyMonth',{templateUrl:'studyMonth.html'})
+            .when('/writeExperience',{templateUrl:'writeException.html'})
+            .when('/studyReport',{templateUr:'studyReport.html'})
+            .when('/test',{templateUrl:'test.html'})
+
+    });
